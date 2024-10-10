a6c35968e4a8c73b668085ec780a09f9ac9f2426
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2024-10-09 21:34:31 -0400

    First completed code
    Could be a little overly complicated and long.

--- cs50python/week4/professor.py
+++ cs50python/week4/professor.py
@@ -1,16 +1,67 @@
 import random
 
-def main(): 
-    n = get_level()
+# Loop: User is prompted a math problem, user inputs answer, new problem (10 times)
 
+def main():     
+    level = get_level()
+    questions_amount = 1
+    correct_answer = 0
+    incorrect_answer = 0
+    while questions_amount <= 10:
+        try:
+            x, y, answer = generate_integer(level)
+            print(answer)
+            user_answer = int(input(f"{x} + {y} = "))
+            if user_answer == answer:
+                correct_answer += 1
+                questions_amount += 1
+                if questions_amount == 10:
+                    break
+            while user_answer != answer:
+                print("EEE")
+                incorrect_answer += 1
+                user_answer = int(input(f"{x} + {y} = "))
+                if user_answer == answer:
+                    break
+                if incorrect_answer == 3:
+                    print(f"{x} + {y} = {answer}")
+                    break
+        except ValueError:
+            print("EEE")
+                incorrect_answer += 1
+                user_answer = int(input(f"{x} + {y} = "))
+                if user_answer == answer:
+                    break
+                if incorrect_answer == 3:
+                    print(f"{x} + {y} = {answer}")
+                    break
+
+    print(f"Score: {correct_answer}")
 
 def get_level():
     while True:
-        n = input("Level: ")
-        if n == 1 or n == 2 or n == 3:
-            return n
+        try:
+            n = int(input("Level: "))
+            if n == 1 or n == 2 or n == 3:
+                return n
+        except ValueError:
+            pass
 
 def generate_integer(level):
+    # X and Y has n digits 
+    if level == 1:
+        x = random.randint(0,9)
+        y = random.randint(0,9)
+        answer = x + y 
+    elif level == 2:
+        x = random.randint(0,99)
+        y = random.randint(0,99)
+        answer = x + y
+    elif level == 3:
+        x = random.randint(0,999)
+        y = random.randint(0,999)
+        answer = x + y
+    return x, y, answer
 
 
 if __name__ == "__main__":
