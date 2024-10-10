d46560f9fd88f2bcdfb829609e8bfee5aba2b8aa
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2024-10-09 21:05:28 -0400

    Update professor.py

--- cs50python/week4/professor.py
+++ cs50python/week4/professor.py
@@ -1,10 +1,14 @@
 import random
 
-def main():
-    
+def main(): 
+    n = get_level()
+
 
 def get_level():
-
+    while True:
+        n = input("Level: ")
+        if n == 1 or n == 2 or n == 3:
+            return n
 
 def generate_integer(level):
 
